ed2264851e80a0c120a16b51acdbad7a1420fccb
akhileshsude <akhileshsude656@gmail.com>
2026-09-24 00:15:37 +0530

    adding another get

--- app.js
+++ app.js
@@ -74,6 +74,20 @@ app.delete('/assignments/:id', async(req, res)=>{
     }
 })
 
+app.get('/assignments', async(req, res)=>{
+    try {
+        const { submitted } = req.params;
+        let result = await pool.query(`
+            SELECT * from assignments where submitted=$1
+            returning *;`, [submitted])
+        res.status(200).json(result.rows[0])
+    } catch(err) {
+        console.log(err)
+        res.status(500).json({
+            errorMessage: 'Server is stop'
+        });
+    }
+})
 app.listen(PORT, () => {
     console.log('Server running on port 3000');
 })
